191fa6a733aad3ca33c8cf35ebbb723a44eaff1e
Daniel Angeles <daniel.angeles9806@gmail.com>
2023-09-24 19:56:12 -0500

    Add fetch to list of products

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useSWR from 'swr'
+import { type Product } from './types/product.ts'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = (): JSX.Element => {
+  const { data, isLoading } = useSWR<Product[]>('https://fakestoreapi.com/products',
+    async (url) => {
+      const response = await fetch(url)
+      const data = await response.json()
+      return data
+    }
+  )
+
+  if (isLoading) return <h1>Cargando...</h1>
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Hola mundo</h1>
+      <p>
+        {JSON.stringify(data)}
       </p>
     </>
   )
